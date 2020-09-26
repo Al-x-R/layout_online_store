25d496b7fcc50c7885471d7392faaf8c9aadf122
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-09-26 23:44:58 +0300

    Add banner section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn">' +
+            '<img src="images/left.svg" alt=""></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnNext">' +
+            '<img src="images/right.svg" alt=""></button>'
+    });
 });
