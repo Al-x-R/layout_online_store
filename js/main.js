0e0bfa01792b927046ce084624c9a654ad1bf78c
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-09-30 21:41:20 +0300

    Fix favorite switching

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     $('.banner-section__slider').slick({
         dots: true,
         prevArrow: '<button class="banner-section__slider-btn">' +
@@ -19,6 +19,32 @@ $(function(){
     })
 
     $('.product-item__favorite').on('click', function () {
-        $('.product-item__favorite').toggleClass('product-item__favorite--active')
+        $(this).toggleClass('product-item__favorite--active')
     })
 });
+
+// const tabNav = document.querySelectorAll('.search__tabs-item')
+// const tabContent = document.querySelectorAll('.search__content-item')
+//
+// tabNav.forEach(item => {
+//     item.addEventListener('click', selectTab)
+// })
+//
+// function selectTab() {
+//     let tabName;
+//     tabNav.forEach(item => {
+//         item.classList.remove('tabs-content--active')
+//     })
+//     this.classList.add('tabs-content--active')
+//     tabName = this.getAttribute('href')
+//     selectTabContent(tabName)
+// }
+//
+// function selectTabContent(tabName) {
+//     tabContent.forEach(item => {
+//         item.classList.contains(tabName) ?
+//             item.classList.add('tabs-content--active') :
+//             item.classList.remove('tabs-content--active')
+//     })
+// }
+
