9d9d305796a79a0e3c7057deb3281591b913a268
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-09-30 21:45:45 +0300

    Finish products section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function () {
     $('.banner-section__slider').slick({
         dots: true,
-        prevArrow: '<button class="banner-section__slider-btn">' +
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnPrev">' +
             '<img src="images/left.svg" alt=""></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnNext">' +
             '<img src="images/right.svg" alt=""></button>'
